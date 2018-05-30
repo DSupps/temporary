99ff08dae4e9401ee76ee90cb6f4c414ad9408cd
David Supple <davidasupple@gmail.com>
2018-05-29 20:07:01 -0400

    extra practice problems chapter 2

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,13 @@
-# this is just a comment
+# these are the extra practice problems
+my_vec <- c(5, 10, 15, 20, 25)
+twicemy_vec <- my_vec * 2
+newmy_vec <- c(2, 4, 6, 8, 10)
+tenmynew_vec <- newmy_vec + 10
+my_names <- c("Dasher", "Prancer", "Dancer", "Vixen", "Rudolph")
+davids_df <- data.frame(my_vec, twicemy_vec, newmy_vec, tennewmy_vec, my_names)
+davids_df <- arrange(davids_df, my_names)
+positions <- order(davids_df$my_names)
+davids_df[positions, ]
+
+str(davids_df)
+
